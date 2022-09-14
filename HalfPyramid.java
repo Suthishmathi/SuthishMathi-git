a5bc53a62701f9b335602ce9fd2dc459d3bffd09
Suthishmathi <msdvijayarasan426@gmail.com>
2022-09-14 13:11:37 +0530

    Create HalfPyramid.java

---
+++ HalfPyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class HalfPyramid
+{
+	public static void main(String[] args)
+  {
+	int i,j;
+	System.out.println("Enter the number of rows");
+		Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<i;j++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+  }
+}
