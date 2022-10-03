ed96e0ec707be1e1e0c0a8a6bd38cd3ee473791d
Suthishmathi <102375441+Suthishmathi@users.noreply.github.com>
2022-10-03 18:17:17 +0530

    Add files via upload

--- LoginModule.java
+++ LoginModule.java
@@ -43,6 +43,7 @@ class Admin
 	Scanner sc2=new Scanner(System.in);
 	String book[]=new String[50];
 	int id[]=new int[50];
+	int cost[]=new int[50];
 	public void content() {
 		System.out.println(" 1. Add Book in a counter \n 2.View a Book \n 3.Update \n 4.Delete Book");
 		int get=sc2.nextInt();
@@ -76,28 +77,88 @@ class Admin
 					book[i]=sc2.nextLine();
 			System.out.println("Enter the Book Name");
 				book[i]=sc2.nextLine();
+				System.out.println("Enter the Book Cost");
+				cost[i]=sc2.nextInt();
 		}
 		content();
 	}
 	public void view()
 	{
-		System.out.println("SI.No \t Book ID \t Book Name ");
+		System.out.println("SI.No \t Book ID \t Book Name \t Cost");
 		book[0]="Ponniyin Selvan";
 		id[0]=1;
 		id[1]=2;
 		book[1]="IronMan";
+		cost[0]=250;
+		cost[1]=200;
 		byte s=0;
 		for(int i=0;i<book.length;i++)
 		{
-			s++;
-			if(book[i]!=null&& id[i]!=0)
-			System.out.println(s+".     \t"+id[i]+"     \t "+book[i]);
+			
+			if(book[i]!=null&& id[i]!=0&&cost[i]!=0)
+			{
+				s++;
+			System.out.println(s+".     \t"+id[i]+"     \t     "+book[i]+"         \t    "+cost[i]);
+			}
 		}
 		content();
 	}
 	public void update()
 	{
-		System.out.println("Enter 
+		System.out.println("Enter the 1.Book ID or 2.Book name 3.Cost  to change");
+		int choice1=sc2.nextByte();
+		
+		
+		switch(choice1){
+			case 1:
+			{	
+				System.out.println("Enter the book id to change ");
+				int id2=sc2.nextInt();
+				for(int i=0;i<book.length;i++)
+					{
+						if(id[i]==id2)
+						{
+							System.out.println("Enter the new Book id");
+						id[i]=sc2.nextInt();
+						break;
+						}	
+					}
+					content();	
+			}
+			break;
+			case 2:
+			{	
+				System.out.println("Enter the book name to change ");
+				String book1=sc2.next();
+				for(int i=0;i<book.length;i++)
+					{
+						if(book[i]==book1)
+						{
+							System.out.println("Enter the new Book name");
+						book[i]=sc2.next();
+						break;
+						}	
+					}
+					content();	
+			}
+			break;
+			case 3:
+			{	
+				System.out.println("Enter the book cost to change ");
+				int cost1=sc2.nextInt();
+				for(int i=0;i<book.length;i++)
+					{
+						if(cost[i]==cost1)
+						{
+							System.out.println("Enter the new Book cost");
+						cost[i]=sc2.nextInt();
+						break;
+						}	
+					}
+					content();	
+			}
+			break;
+		}
 	}
 	public void  deleteBook()
 	{
@@ -105,11 +166,13 @@ class Admin
 		 byte id1=sc2.nextByte();
 		 for(int i=0;i<book.length;i++)
 		 {
-			 if(id[i]=id1)
+			 if(id[i]==id1)
 			 {
-				 
+				 book[i]=null;
+				 id[i]=0;
 			 }
 		 }
+		 content();
 	}
 }
 
